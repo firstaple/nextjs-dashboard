2f703e60bff0eb3a46c4c3070431f57d312e88f0
firstaple <firstaple@gmail.com>
2024-03-07 02:44:34 +0900

    Streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+// replacement while page content load
+// loading.tsx => static file
+
+import DashboardSkeleton from '../../ui/skeletons';
+
+export default function Loading() {
+  // simplified version of the UI
+  return <DashboardSkeleton />;
+}
